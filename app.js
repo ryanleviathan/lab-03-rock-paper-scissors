78e21b2b8e1b006fce61ac984be1e887ec4584b1
ryanleviathan <ryan.m.carreras@gmail.com>
2020-09-30 15:41:30 -0700

    HTML done, JS halfway

--- app.js
+++ app.js
@@ -1,5 +1,74 @@
 // import functions and grab DOM elements
 
-// initialize state
+const play = document.getElementById('play');
+const reset = document.getElementById('reset');
+
+const winsSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+const tiesSpan = document.getElementById('ties');
+const resultSpan = document.getElementById('results');
+const resetSpan = document.getElementById('reset');
+
+// set initial state
+
+let winTotal = 0;
+let lossTotal = 0;
+let tieTotal = 0;
+let resetTotal = 0;
+
+// define DOM utitlies
+
+const getZeroOneOrTwo = () => Math.round(Math.random() * 2);
+
+const getNpcChoice = (zeroOneOrTwo) => {
+    if (zeroOneOrTwo === 0) return 'rock';
+    if (zeroOneOrTwo === 1) return 'paper';
+    if (zeroOneOrTwo === 2) return 'scissors';
+};
+
+const userWon = 
+
+const userLost =
+
+const userTied =
+
+
+
+
+
+
+
+
+
+//const playAgainstNpc = () => {
+    //values
+    //const selectedRadioButton = document.querySelector('input:checked');
+    //const userInput = selectedRadioButton.nodeValue;
+    //changes to state and DOM
+    //const zeroOneOrTwo = (getZeroOneOrTwo);
+    //const randomNpcChoice = (getNpcChoice);
+    //const won = userWon(userInput, randomNpcChoice);
+    //const lost = userLost(userInput, ran)
+    //cosnt tied =
+
+    //if (won) {
+    //    winTotal++;
+    //}
+
+    //if ()
+//}
+
+play.addEventListener('click', () => {
+
+});
+
+
+reset.addEventListener('click', () => {
+    winTotal = 0;
+    lossTotal = 0;
+    tieTotal = 0;
+    resetTotal++;
+});
 
 // set event listeners to update state and DOM
+
